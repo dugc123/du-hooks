7a5d39a0d15475929e2a65c596a81221973608ff
dugengchen <dugengchen6@163.com>
2025-07-23 21:09:55 +0800

    test

---
+++ docs-dist/meta__docs.33274bc9.async.js
@@ -0,0 +1,118 @@
+"use strict";(self.webpackChunkdu_hooks=self.webpackChunkdu_hooks||[]).push([[904],{29599:function(l,n,e){e.r(n),e.d(n,{demos:function(){return r}});var t=e(75271),r={}},25194:function(l,n,e){var t;e.r(n),e.d(n,{demos:function(){return v}});var r=e(90228),s=e.n(r),_=e(87999),p=e.n(_),d=e(75271),h=e(38255),v={"docs-hooks-use-boolean-demo-demo":{component:d.memo(d.lazy(function(){return e.e(433).then(e.bind(e,62104))})),asset:{type:"BLOCK",id:"docs-hooks-use-boolean-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(43544).Z},"du-hooks":{type:"NPM",value:"0.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"du-hooks":h,react:t||(t=e.t(d,2))},renderOpts:{compile:function(){var m=p()(s()().mark(function I(){var a,g=arguments;return s()().wrap(function(u){for(;;)switch(u.prev=u.next){case 0:return u.next=2,e.e(148).then(e.bind(e,64148));case 2:return u.abrupt("return",(a=u.sent).default.apply(a,g));case 3:case"end":return u.stop()}},I)}));function i(){return m.apply(this,arguments)}return i}()}}}},54177:function(l,n,e){var t;e.r(n),e.d(n,{demos:function(){return m}});var r=e(90228),s=e.n(r),_=e(87999),p=e.n(_),d=e(75271),h=e(58027),v=e(38255),m={"docs-hooks-use-mount-demo-demo":{component:d.memo(d.lazy(function(){return e.e(433).then(e.bind(e,70146))})),asset:{type:"BLOCK",id:"docs-hooks-use-mount-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(88762).Z},antd:{type:"NPM",value:"5.26.6"},"du-hooks":{type:"NPM",value:"0.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{antd:h,"du-hooks":v,react:t||(t=e.t(d,2))},renderOpts:{compile:function(){var i=p()(s()().mark(function a(){var g,f=arguments;return s()().wrap(function(D){for(;;)switch(D.prev=D.next){case 0:return D.next=2,e.e(148).then(e.bind(e,64148));case 2:return D.abrupt("return",(g=D.sent).default.apply(g,f));case 3:case"end":return D.stop()}},a)}));function I(){return i.apply(this,arguments)}return I}()}}}},14464:function(l,n,e){var t;e.r(n),e.d(n,{demos:function(){return v}});var r=e(90228),s=e.n(r),_=e(87999),p=e.n(_),d=e(75271),h=e(38255),v={"docs-hooks-use-toggle-demo-demo1":{component:d.memo(d.lazy(function(){return e.e(433).then(e.bind(e,66979))})),asset:{type:"BLOCK",id:"docs-hooks-use-toggle-demo-demo1",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(45478).Z},"du-hooks":{type:"NPM",value:"0.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"du-hooks":h,react:t||(t=e.t(d,2))},renderOpts:{compile:function(){var m=p()(s()().mark(function I(){var a,g=arguments;return s()().wrap(function(u){for(;;)switch(u.prev=u.next){case 0:return u.next=2,e.e(148).then(e.bind(e,64148));case 2:return u.abrupt("return",(a=u.sent).default.apply(a,g));case 3:case"end":return u.stop()}},I)}));function i(){return m.apply(this,arguments)}return i}()}},"docs-hooks-use-toggle-demo-demo2":{component:d.memo(d.lazy(function(){return e.e(433).then(e.bind(e,70876))})),asset:{type:"BLOCK",id:"docs-hooks-use-toggle-demo-demo2",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(28889).Z},"du-hooks":{type:"NPM",value:"0.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u5728\u4EFB\u610F\u4E24\u4E2A\u503C\u4E4B\u95F4\u5207\u6362"},context:{"du-hooks":h,react:t||(t=e.t(d,2))},renderOpts:{compile:function(){var m=p()(s()().mark(function I(){var a,g=arguments;return s()().wrap(function(u){for(;;)switch(u.prev=u.next){case 0:return u.next=2,e.e(148).then(e.bind(e,64148));case 2:return u.abrupt("return",(a=u.sent).default.apply(a,g));case 3:case"end":return u.stop()}},I)}));function i(){return m.apply(this,arguments)}return i}()}}}},38887:function(l,n,e){e.r(n),e.d(n,{demos:function(){return h}});var t=e(90228),r=e.n(t),s=e(87999),_=e.n(s),p=e(75271),d=e(38255),h={"docs-hooks-use-unmount-demo-0":{component:p.memo(p.lazy(_()(r()().mark(function v(){var m,i;return r()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,Promise.resolve().then(e.bind(e,38255));case 2:return m=a.sent,i=m.useUnmount,a.abrupt("return",{default:function(){return p.createElement("useUnmount",{title:"Hello dumi!"})}});case 5:case"end":return a.stop()}},v)})))),asset:{type:"BLOCK",id:"docs-hooks-use-unmount-demo-0",refAtomIds:[],dependencies:{"index.jsx":{type:"FILE",value:`import { useUnmount } from 'du-hooks';
+
+export default () => <useUnmount title="Hello dumi!" />;`},"du-hooks":{type:"NPM",value:"0.0.1"}},entry:"index.jsx"},context:{"du-hooks":d},renderOpts:{compile:function(){var v=_()(r()().mark(function i(){var I,a=arguments;return r()().wrap(function(f){for(;;)switch(f.prev=f.next){case 0:return f.next=2,e.e(148).then(e.bind(e,64148));case 2:return f.abrupt("return",(I=f.sent).default.apply(I,a));case 3:case"end":return f.stop()}},i)}));function m(){return v.apply(this,arguments)}return m}()}}}},59157:function(l,n,e){e.r(n),e.d(n,{demos:function(){return r}});var t=e(75271),r={}},38255:function(l,n,e){e.r(n),e.d(n,{useBoolean:function(){return d},useMount:function(){return A},useToggle:function(){return p},useUnmount:function(){return W}});var t=e(48305),r=e.n(t),s=e(75271);function _(){var c=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!1,o=arguments.length>1?arguments[1]:void 0,x=(0,s.useState)(c),M=r()(x,2),O=M[0],E=M[1],y=(0,s.useMemo)(function(){var T=o===void 0?!c:o,U=function(){return E(function(C){return C===c?T:c})},b=function(C){return E(C)},P=function(){return E(c)},R=function(){return E(T)};return{toggle:U,set:b,setLeft:P,setRight:R}},[]);return[O,y]}var p=_;function d(){var c=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!1,o=p(!!c),x=r()(o,2),M=x[0],O=x[1],E=O.toggle,y=O.set,T=(0,s.useMemo)(function(){var U=function(){return y(!0)},b=function(){return y(!1)};return{toggle:E,set:function(R){return y(!!R)},setTrue:U,setFalse:b}},[]);return[M,T]}var h=e(31759),v=e.n(h),m=!1,i=m,I=function(o){return o!==null&&_typeof(o)==="object"},a=function(o){return typeof o=="function"},g=function(o){return typeof o=="string"},f=function(o){return typeof o=="boolean"},u=function(o){return typeof o=="number"},D=function(o){return typeof o=="undefined"},B=function(o){i&&(a(o)||console.error('useMount: parameter `fn` expected to be a function, but got "'.concat(v()(o),'".'))),(0,s.useEffect)(function(){o==null||o()},[])},A=B,K=function(o){i&&(a(o)||console.error("useUnmount expected parameter is a function, got ".concat(v()(o))));var x=(0,s.useRef)(o);(0,s.useEffect)(function(){return function(){x.current()}},[])},W=K},13519:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[]},10547:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"\u4F18\u96C5\u7684\u7BA1\u7406 boolean \u72B6\u6001\u7684 Hook\u3002",paraId:0,tocIndex:0},{value:`const [state, { toggle, set, setTrue, setFalse }] = useBoolean(
+  defaultValue?: boolean,
+);
+`,paraId:1,tocIndex:3},{value:"\u53C2\u6570",paraId:2,tocIndex:4},{value:"\u8BF4\u660E",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"defaultValue",paraId:2,tocIndex:4},{value:"\u53EF\u9009\u9879\uFF0C\u4F20\u5165\u9ED8\u8BA4\u7684\u72B6\u6001\u503C",paraId:2,tocIndex:4},{value:"boolean",paraId:2,tocIndex:4},{value:"false",paraId:2,tocIndex:4},{value:"\u53C2\u6570",paraId:3,tocIndex:5},{value:"\u8BF4\u660E",paraId:3,tocIndex:5},{value:"\u7C7B\u578B",paraId:3,tocIndex:5},{value:"state",paraId:3,tocIndex:5},{value:"\u72B6\u6001\u503C",paraId:3,tocIndex:5},{value:"boolean",paraId:3,tocIndex:5},{value:"actions",paraId:3,tocIndex:5},{value:"\u64CD\u4F5C\u96C6\u5408",paraId:3,tocIndex:5},{value:"Actions",paraId:3,tocIndex:5},{value:"\u53C2\u6570",paraId:4,tocIndex:6},{value:"\u8BF4\u660E",paraId:4,tocIndex:6},{value:"\u7C7B\u578B",paraId:4,tocIndex:6},{value:"toggle",paraId:4,tocIndex:6},{value:"\u5207\u6362 state",paraId:4,tocIndex:6},{value:"() => void",paraId:4,tocIndex:6},{value:"set",paraId:4,tocIndex:6},{value:"\u8BBE\u7F6E state",paraId:4,tocIndex:6},{value:"(value: boolean) => void",paraId:4,tocIndex:6},{value:"setTrue",paraId:4,tocIndex:6},{value:"\u8BBE\u7F6E\u4E3A true",paraId:4,tocIndex:6},{value:"() => void",paraId:4,tocIndex:6},{value:"setFalse",paraId:4,tocIndex:6},{value:"\u8BBE\u7F6E\u4E3A false",paraId:4,tocIndex:6},{value:"() => void",paraId:4,tocIndex:6}]},42802:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"\u53EA\u5728\u7EC4\u4EF6\u521D\u59CB\u5316\u65F6\u6267\u884C\u7684 Hook\u3002",paraId:0,tocIndex:0},{value:`useMount(fn: () => void);
+`,paraId:1,tocIndex:3},{value:"\u53C2\u6570",paraId:2,tocIndex:4},{value:"\u8BF4\u660E",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"fn",paraId:2,tocIndex:4},{value:"\u521D\u59CB\u5316\u65F6\u6267\u884C\u7684\u51FD\u6570",paraId:2,tocIndex:4},{value:"() => void",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4}]},61444:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"\u7528\u4E8E\u5728\u4E24\u4E2A\u72B6\u6001\u503C\u95F4\u5207\u6362\u7684 Hook\u3002",paraId:0,tocIndex:0},{value:`const [state, { toggle, set, setLeft, setRight }] = useToggle(defaultValue?: boolean);
+
+const [state, { toggle, set, setLeft, setRight }] = useToggle<T>(defaultValue: T);
+
+const [state, { toggle, set, setLeft, setRight }] = useToggle<T, U>(defaultValue: T, reverseValue: U);
+`,paraId:1,tocIndex:5},{value:"\u53C2\u6570",paraId:2,tocIndex:6},{value:"\u8BF4\u660E",paraId:2,tocIndex:6},{value:"\u7C7B\u578B",paraId:2,tocIndex:6},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:6},{value:"defaultValue",paraId:2,tocIndex:6},{value:"\u53EF\u9009\u9879\uFF0C\u4F20\u5165\u9ED8\u8BA4\u7684\u72B6\u6001\u503C",paraId:2,tocIndex:6},{value:"T",paraId:2,tocIndex:6},{value:"false",paraId:2,tocIndex:6},{value:"reverseValue",paraId:2,tocIndex:6},{value:"\u53EF\u9009\u9879\uFF0C\u4F20\u5165\u53D6\u53CD\u7684\u72B6\u6001\u503C",paraId:2,tocIndex:6},{value:"U",paraId:2,tocIndex:6},{value:"-",paraId:2,tocIndex:6},{value:"\u53C2\u6570",paraId:3,tocIndex:7},{value:"\u8BF4\u660E",paraId:3,tocIndex:7},{value:"\u7C7B\u578B",paraId:3,tocIndex:7},{value:"state",paraId:3,tocIndex:7},{value:"\u72B6\u6001\u503C",paraId:3,tocIndex:7},{value:"-",paraId:3,tocIndex:7},{value:"actions",paraId:3,tocIndex:7},{value:"\u64CD\u4F5C\u96C6\u5408",paraId:3,tocIndex:7},{value:"Actions",paraId:3,tocIndex:7},{value:"\u53C2\u6570",paraId:4,tocIndex:8},{value:"\u8BF4\u660E",paraId:4,tocIndex:8},{value:"\u7C7B\u578B",paraId:4,tocIndex:8},{value:"toggle",paraId:4,tocIndex:8},{value:"\u5207\u6362 state",paraId:4,tocIndex:8},{value:"() => void",paraId:4,tocIndex:8},{value:"set",paraId:4,tocIndex:8},{value:"\u4FEE\u6539 state",paraId:4,tocIndex:8},{value:"(state: T | U) => void",paraId:4,tocIndex:8},{value:"setLeft",paraId:4,tocIndex:8},{value:"\u8BBE\u7F6E\u4E3A defaultValue",paraId:4,tocIndex:8},{value:"() => void",paraId:4,tocIndex:8},{value:"setRight",paraId:4,tocIndex:8},{value:"\u5982\u679C\u4F20\u5165\u4E86 reverseValue, \u5219\u8BBE\u7F6E\u4E3A reverseValue\u3002 \u5426\u5219\u8BBE\u7F6E\u4E3A defaultValue \u7684\u53CD\u503C",paraId:4,tocIndex:8},{value:"() => void",paraId:4,tocIndex:8}]},39236:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"This is an example component.",paraId:0,tocIndex:0}]},15206:function(l,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[]},43544:function(l,n){n.Z=`import { useBoolean } from 'du-hooks';
+import React from 'react';
+
+export default () => {
+  const [state, { toggle, setTrue, setFalse }] = useBoolean(true);
+
+  return (
+    <div>
+      <p>Effects\uFF1A{JSON.stringify(state)}</p>
+      <p>
+        <button type="button" onClick={toggle}>
+          Toggle
+        </button>
+        <button type="button" onClick={setFalse} style={{ margin: '0 16px' }}>
+          Set false
+        </button>
+        <button type="button" onClick={setTrue}>
+          Set true
+        </button>
+      </p>
+    </div>
+  );
+};
+`},88762:function(l,n){n.Z=`import { message } from 'antd';
+import { useBoolean, useMount } from 'du-hooks';
+import React from 'react';
+
+const MyComponent = () => {
+  useMount(() => {
+    message.info('mount');
+  });
+
+  return <div>Hello World</div>;
+};
+
+export default () => {
+  const [state, { toggle }] = useBoolean(false);
+
+  return (
+    <>
+      <button type="button" onClick={toggle}>
+        {state ? 'unmount' : 'mount'}
+      </button>
+      {state && <MyComponent />}
+    </>
+  );
+};
+`},45478:function(l,n){n.Z=`import { useToggle } from 'du-hooks';
+import React from 'react';
+
+export default () => {
+  const [state, { toggle, setLeft, setRight }] = useToggle();
+
+  return (
+    <div>
+      <p>Effects\uFF1A{\`\${state}\`}</p>
+      <p>
+        <button type="button" onClick={toggle}>
+          Toggle
+        </button>
+        <button type="button" onClick={setLeft} style={{ margin: '0 8px' }}>
+          Toggle False
+        </button>
+        <button type="button" onClick={setRight}>
+          Toggle True
+        </button>
+      </p>
+    </div>
+  );
+};
+`},28889:function(l,n){n.Z=`import { useToggle } from 'du-hooks';
+import React from 'react';
+
+export default () => {
+  const [state, { toggle, set, setLeft, setRight }] = useToggle(
+    'Hello',
+    'World',
+  );
+
+  return (
+    <div>
+      <p>Effects\uFF1A{state}</p>
+      <p>
+        <button type="button" onClick={toggle}>
+          Toggle
+        </button>
+        <button
+          type="button"
+          onClick={() => set('Hello')}
+          style={{ margin: '0 8px' }}
+        >
+          Set Hello
+        </button>
+        <button type="button" onClick={() => set('World')}>
+          Set World
+        </button>
+        <button type="button" onClick={setLeft} style={{ margin: '0 8px' }}>
+          Set Left
+        </button>
+        <button type="button" onClick={setRight}>
+          Set Right
+        </button>
+      </p>
+    </div>
+  );
+};
+`}}]);
